705f6702da7e603d0022320bf6a74701c0e53f07
Matthew Roche <MJRoche@usf.edu>
2015-10-15 22:49:34 -0400

    workflor for editing person

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source 'https://rubygems.org'
 
-gem 'capybara'
+
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.4'
@@ -42,6 +42,7 @@ end
 group :development do
   # Access an IRB console on exception pages or by using <%= console %> in views
   gem 'web-console', '~> 2.0'
+  gem 'capybara'
 
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
--- spec/controllers/phone_numbers_controller_spec.rb
+++ spec/controllers/phone_numbers_controller_spec.rb
@@ -106,16 +106,20 @@ RSpec.describe PhoneNumbersController, type: :controller do
 
   describe "PUT #update" do
     context "with valid params" do
+      let(:bob) { Person.create(first_name: 'Bob', last_name: 'Jones') }
+      let(:valid_attributes) { {number: '555-5678', person_id: bob.id} }
+      let(:new_attributes) { {number: 'MyNewString', person_id: bob.id} }
+
       let(:new_attributes) {
         {number: 'MyNewString', person_id: 2}
       }
 
-      it "updates the requested phone_number" do
+      it "redirects to the phone_number" do
+        bob = Person.create(first_name: 'Bob', last_name: 'Jones')
+        valid_attributes = {number: '555-5678', person_id: bob.id}
         phone_number = PhoneNumber.create! valid_attributes
-        put :update, {:id => phone_number.to_param, :phone_number => new_attributes}, valid_session
-        phone_number.reload
-        expect(phone_number.number).to eq('MyNewString')
-        expect(phone_number.person_id).to eq(2)
+        put :update, {:id => phone_number.to_param, :phone_number => valid_attributes}, valid_session
+        expect(response).to redirect_to(bob)
       end
 
       it "assigns the requested phone_number as @phone_number" do
@@ -125,9 +129,11 @@ RSpec.describe PhoneNumbersController, type: :controller do
       end
 
       it "redirects to the phone_number" do
+        bob = Person.create(first_name: 'Bob', last_name: 'Jones')
+        valid_attributes = {number: '555-5678', person_id: bob.id}
         phone_number = PhoneNumber.create! valid_attributes
         put :update, {:id => phone_number.to_param, :phone_number => valid_attributes}, valid_session
-        expect(response).to redirect_to(phone_number)
+        expect(response).to redirect_to(bob)
       end
     end
 
